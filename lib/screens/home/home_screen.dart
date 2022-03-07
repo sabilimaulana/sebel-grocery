088ef15dd9eb7dee60d48f41306bea690a3104a7
sabilimaulana <spacysbl.learn@gmail.com>
2022-03-07 23:08:35 +0700

    Home Page

--- lib/screens/home/home_screen.dart
+++ lib/screens/home/home_screen.dart
@@ -16,41 +16,62 @@ class HomeScreen extends StatelessWidget {
         bottom: false,
         child: Container(
           color: const Color(0xFFEAEAEA),
-          child: Column(
-            children: [
-              const HomeHeader(),
-              Expanded(
-                child: Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: defaultPadding),
-                  decoration: const BoxDecoration(
-                    color: Colors.white,
-                    borderRadius: BorderRadius.only(
-                      bottomLeft: Radius.circular(defaultPadding * 1.5),
-                      bottomRight: Radius.circular(defaultPadding * 1.5),
+          child: LayoutBuilder(builder: (
+            context,
+            BoxConstraints constraints,
+          ) {
+            return Stack(
+              children: [
+                const Positioned(
+                  top: 0,
+                  right: 0,
+                  left: 0,
+                  height: headerHeight,
+                  child: HomeHeader(),
+                ),
+                Positioned(
+                  top: headerHeight,
+                  left: 0,
+                  right: 0,
+                  height: constraints.maxHeight - headerHeight - cartBarHeight,
+                  child: Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: defaultPadding),
+                    decoration: const BoxDecoration(
+                      color: Colors.white,
+                      borderRadius: BorderRadius.only(
+                        bottomLeft: Radius.circular(defaultPadding * 1.5),
+                        bottomRight: Radius.circular(defaultPadding * 1.5),
+                      ),
                     ),
-                  ),
-                  child: GridView.builder(
-                    itemCount: demoProducts.length,
-                    gridDelegate:
-                        const SliverGridDelegateWithFixedCrossAxisCount(
-                      crossAxisCount: 2,
-                      childAspectRatio: 0.75,
-                      mainAxisSpacing: defaultPadding,
-                      crossAxisSpacing: defaultPadding,
-                    ),
-                    itemBuilder: (context, index) => ProductCard(
-                      product: demoProducts[index],
-                      press: () {},
+                    child: GridView.builder(
+                      itemCount: demoProducts.length,
+                      gridDelegate:
+                          const SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: 2,
+                        childAspectRatio: 0.75,
+                        mainAxisSpacing: defaultPadding,
+                        crossAxisSpacing: defaultPadding,
+                      ),
+                      itemBuilder: (context, index) => ProductCard(
+                        product: demoProducts[index],
+                        press: () {},
+                      ),
                     ),
                   ),
                 ),
-              ),
-              const SizedBox(
-                height: cartBarHeight,
-              )
-            ],
-          ),
+                Positioned(
+                  bottom: 0,
+                  left: 0,
+                  right: 0,
+                  height: cartBarHeight,
+                  child: Container(
+                    color: Colors.red,
+                  ),
+                )
+              ],
+            );
+          }),
         ),
       ),
     );
